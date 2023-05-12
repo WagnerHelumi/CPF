9b026f611d53ba62503c1b53984d68137f062a03
WagnerHelumi <132676933+WagnerHelumi@users.noreply.github.com>
2023-05-12 13:59:09 -0300

    Delete ex004.py

--- Secao03/ex004.py
+++
@@ -1,7 +0,0 @@
-nome=input('Digite seu nome ')
-if len(nome) <= 4:
-    print('Seu nome é curto')
-if len(nome) >= 5 and len(nome) <=6:
-    print('Seu nome é normal')
-if len(nome) > 6:
-    print('Seu nome é grande')
