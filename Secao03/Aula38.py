381348930cc420e61d38119a4604ddbae9abe28b
WagnerHelumi <132676933+WagnerHelumi@users.noreply.github.com>
2023-05-12 13:58:43 -0300

    Delete Aula38.py

--- Secao03/Aula38.py
+++
@@ -1,6 +0,0 @@
-primeiro_Valor=input(('Digite um número '))
-segundo_valor=input(('Digite outro valor '))
-if primeiro_Valor > segundo_valor:
-    print('O {} é maior que {}'.format(primeiro_Valor, segundo_valor))
-else:
-    print('O {} é maior que {}'.format(segundo_valor, primeiro_Valor))
